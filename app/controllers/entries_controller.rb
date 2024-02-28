b085e67ba539f134c46a1c3b609abdff44d14b17
Cecily Ding <cecily.ding@kellogg.northwestern.edu>
2024-02-28 05:35:32 +0000

    P2

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,23 +1,30 @@
 class EntriesController < ApplicationController
-  def show
-    @entry = Entry.find(params[:id])
-    @place = @entry.place
-  end
+  before_action :set_place, only: [:new, :create]
+  before_action :set_entry, only: [:create]
 
   def new
     @entry = Entry.new
+    @entry.place_id = @place.id
   end
 
   def create
     @entry = Entry.new(entry_params)
     if @entry.save
-      redirect_to place_path(@entry.place)
+      redirect_to place_path(@place), notice: 'Entry was successfully created.'
     else
-      render 'new'
+      render :new
     end
   end
-  
+
   private
+
+  def set_place
+    @place = Place.find(params[:place_id])
+  end
+
+  def set_entry
+    @entry = Entry.new(entry_params)
+  end
 
   def entry_params
     params.require(:entry).permit(:title, :description, :posted_on, :place_id)
